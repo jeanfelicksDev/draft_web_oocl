2a37c532defeccf67605aad7d8101cd680839d99
jeanfelicksDev <jeanfelicks@gmail.com>
2026-07-15 20:54:20 +0000

    fix: utilise l'import standard de @prisma/client pour Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../node_modules/@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // Mettre en cache l'instance Prisma dans l'environnement de développement pour éviter les avertissements "Trop de connexions"
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
